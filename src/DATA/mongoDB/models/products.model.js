6c40c1fee4f1502754075576dd08396f2386456a
filippettijuanm <filippetti_juanm@hotmail.com>
2024-01-14 10:29:31 -0300

    final

--- src/DATA/mongoDB/models/products.model.js
+++ src/DATA/mongoDB/models/products.model.js
@@ -5,11 +5,11 @@ import mongoosePaginate from 'mongoose-paginate-v2';
 const productSchema = new mongoose.Schema({
   title: String,
   description: String,
-  code: String,
   price: Number,
-  status: Boolean,
   stock: Number,
+  code: String,
   category: String,
+  status: Boolean,
   thumbnails: [String],
 });
 
